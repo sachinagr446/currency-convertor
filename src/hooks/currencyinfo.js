aac9c87a2f25ee32b23ce2f47fcc7ff10e4d8af4
sachinagr446 <145377162+sachinagr446@users.noreply.github.com>
2024-02-17 02:53:17 +0530

    first commit

---
+++ src/hooks/currencyinfo.js
@@ -0,0 +1,14 @@
+import {useState, useEffect} from "react"
+
+function currencyinfo(currency){
+const [data, setdata] = useState({})
+useEffect(()=>{
+    fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
+    .then((res)=>res.json())
+    .then((res)=>setdata(res[currency]))
+console.log(data)
+
+},[currency])
+return data
+}
+export default currencyinfo;
